4c5dc92b5667d802fb7f41ce029854d17c7c7f26
Tristan Mahé <gled@remote-shell.net>
2018-03-20 17:21:23 -0700

    merge end

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,11 +1,17 @@
 # config/initializers/datadog-tracer.rb
+require 'ddtrace'
 
-Rails.configuration.datadog_trace = {
-  auto_instrument: true,
-  auto_instrument_redis: true,
-  default_service: 'my-rail-app'
-}
 
-#Datadog.configure do |c|
-#	  c.use :rails, options
-#end
+#Rails.configuration.datadog_trace = {
+#  auto_instrument: true,
+#  auto_instrument_redis: true,
+#  default_service: 'my-rail-app'
+#}
+
+Datadog.configure do |c|
+	  c.use :rails
+	  c.use :redis
+	  c.use :sidekiq
+	  c.use :http
+	  c.use :aws
+end
