7c5c9ef567640473b21b5b661fb605918d6cc55a
Tristan Mahé <gled@remote-shell.net>
2018-07-11 14:49:54 -0700

    whoops merge

--- app/javascript/mastodon/features/home_timeline/components/column_settings.js
+++ app/javascript/mastodon/features/home_timeline/components/column_settings.js
@@ -3,13 +3,6 @@ import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import SettingToggle from '../../notifications/components/setting_toggle';
-import SettingText from '../../../components/setting_text';
-
-const messages = defineMessages({
-  filter_regex: { id: 'home.column_settings.filter_regex', defaultMessage: 'Filter out by regular expressions' },
-  settings: { id: 'home.settings', defaultMessage: 'Column settings' },
-  search_home: { id: 'home.search', defaultMessage: 'Search home timeline' },
-});
 
 @injectIntl
 export default class ColumnSettings extends React.PureComponent {
@@ -33,13 +26,6 @@ export default class ColumnSettings extends React.PureComponent {
 
         <div className='column-settings__row'>
           <SettingToggle prefix='home_timeline' settings={settings} settingPath={['shows', 'reply']} onChange={onChange} label={<FormattedMessage id='home.column_settings.show_replies' defaultMessage='Show replies' />} />
-        </div>
-
-        <span className='column-settings__section'><FormattedMessage id='home.column_settings.advanced' defaultMessage='Advanced' /></span>
-
-        <span className='column-settings__section'><FormattedMessage id='home.column_settings.search' defaultMessage='Search my home timeline' /></span>
-        <div className='column-settings__row'>
-          <SettingText prefix='home_timeline' settings={settings} settingKey={['search', 'home']} onChange={onChange} label={intl.formatMessage(messages.search_home)} />
         </div>
       </div>
     );
