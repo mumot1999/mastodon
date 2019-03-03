813a758bf8122f5c374e149c5d2d2bf8980d2ab2
101010 <40118847+101010pl@users.noreply.github.com>
2019-03-03 18:56:36 +0100

    Update start.js

--- public/converse/start.js
+++ public/converse/start.js
@@ -1,6 +1,6 @@
 converse.initialize({
     bosh_service_url: 'https://101010.pl/http-bind/',
-    show_controlbox_by_default: true,
+    show_controlbox_by_default: false,
     allow_chat_pending_contacts: true,
     allow_registration: false,
     auto_reconnect: true,
