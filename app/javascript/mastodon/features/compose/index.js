ae69403e6a2ad732b32932282c920f842af44469
101010 <40118847+101010pl@users.noreply.github.com>
2019-04-01 20:22:54 +0200

    Poprawa ikonek

--- app/javascript/mastodon/features/compose/index.js
+++ app/javascript/mastodon/features/compose/index.js
@@ -92,7 +92,7 @@ class Compose extends React.PureComponent {
             <Link to='/timelines/public' className='drawer__tab' title={intl.formatMessage(messages.public)} aria-label={intl.formatMessage(messages.public)}><Icon id='globe' fixedWidth /></Link>
           )}
           <a href='/settings/preferences' className='drawer__tab' title={intl.formatMessage(messages.preferences)} aria-label={intl.formatMessage(messages.preferences)}><Icon id='cog' fixedWidth /></a>
-          <a href='/auth/sign_out' className='drawer__tab' data-method='delete' title={intl.formatMessage(messages.logout)} aria-label={intl.formatMessage(messages.logout)}><Icon id='sign-out' fixedWidth /></a>
+          <a href='/auth/sign_out' className='drawer__tab' data-method='delete' title={intl.formatMessage(messages.logout)} aria-label={intl.formatMessage(messages.logout)}><Icon id='sign-out-alt' fixedWidth /></a>
         </nav>
       );
     }
