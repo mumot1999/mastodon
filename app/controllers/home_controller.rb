8b7e5a74ad0d5ca83434a4019ab33518320c5d65
gled <gled@remote-shell.net>
2019-10-30 16:47:47 -0700

    check if set_initial_state_json is the issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,6 @@ class HomeController < ApplicationController
   skip_before_action :store_current_location, only: :piwik
   # rubocop sees that as a hash ?!
   before_action :authenticate_user!, :except => %i(piwik) # rubocop:disable Style/HashSyntax
-  before_action :set_initial_state_json, :except => %i(piwik)
   before_action :set_referrer_policy_header
 
   def index
