46d9ce8569e9c6f06572f1f54536ebb3f275b510
gled <gled@remote-shell.net>
2019-10-30 17:46:40 -0700

    more merging with upstream

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,11 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  protect_from_forgery except: :piwik
-  skip_before_action :store_current_location, only: :piwik
-  # rubocop sees that as a hash ?!
-  before_action :authenticate_user!, :except => %i(piwik) # rubocop:disable Style/HashSyntax
+  before_action :authenticate_user!
   before_action :set_referrer_policy_header
 
   def index
     @body_classes = 'app-body'
-  end
-
-  def piwik
-    piwik_user_id =  user_signed_in? ? current_user.id.to_s : ''
-    render js: "<!-- Piwik -->
-  var _paq = _paq || [];
-  /* tracker methods like 'setCustomDimension' should be called before 'trackPageView' */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u='//" + ENV['PIWIK_DOMAIN'] + "/';
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '" + ENV['PIWIK_SITEID'] + "']);
-    _paq.push(['setUserId', '" + piwik_user_id + "']);
-    _paq.push(['trackVisibleContentImpressions']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-<!-- End Piwik Code -->"
   end
 
   private
