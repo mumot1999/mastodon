b9b522acfa5c2147a01dff744923f231a41400f9
101010pl <40118847+101010pl@users.noreply.github.com>
2021-02-05 20:47:41 +0100

    Update content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,8 +8,8 @@ end
 
 base_host = Rails.configuration.x.web_domain
 
-assets_host   = Rails.configuration.action_controller.asset_host "www.comperialead.pl"
-assets_host ||= host_to_url(base_host)
+assets_host   = Rails.configuration.action_controller.asset_host
+assets_host ||= host_to_url(base_host) "www.comperialead.pl"
 
 media_host   = host_to_url(ENV['S3_ALIAS_HOST'])
 media_host ||= host_to_url(ENV['S3_CLOUDFRONT_HOST'])
